3943fa4c4850730b052656bef44d224007a0b076
Carniverous19 <atg23@cornell.edu>
2020-09-25 16:18:55 -0700

    - fixed mixup between tx and rx on poc_reliability table (they were swapped)  - added "bad neighbors" table to v10 report  - updated documentation for new table and clarity

--- README.md
+++ README.md
@@ -38,12 +38,13 @@ For information on PoC v10 requirements see the blog post [Blockchain PoC v10](h
 
 to run the `poc_v10` report run:
 
-    python3 analyze_hotspot.py -x poc_v10 -a {hotspot address}
+    python3 analyze_hotspot.py -x poc_v10 -address {hotspot address}
 
-An example output table is shown below:
+There are two tables in this report.  THe first output table gives a summary overview with an example shown below:
 
     analyzed 202 challenges from 516276-513758
     PoC v10 failures for name-name-name
+    SUMMARY
     Category                   | Total | bad RSSI (%) | bad SNR (%) |
     -----------------------------------------------------------------
     Witnesses to hotspot >300m |   154 |    0 (  0%)  |   13 (  8%) |
@@ -54,21 +55,47 @@ As indicated in the first row, witnesses within 300m meters are ignored as these
 The column "bad RSSI" means the signal violated Free Space Path Loss estimation.  
 The column "bad SNR" means the RSSI was too low for the given SNR.  High SNR but low RSSI means noise is very very low which is suspect.
 
+A second table gives more detail breaking down results by hotspot.
+This doesnt differentiate between types of interaction (witness or PoC hop) but helps to identify if failures are isolated to a few neighbors or across all neighbors (meaning its likely a problem with your hotspot).  
+**Note:** Although I say "bad neighbor" that doesnt mean the hotspot listed is actually gaming, it could be false positives, bad signal between two hotspots due to environmental factors or the reference hotspot that is not at asserted location.
+
+An example table is shown below:
+    
+    BY "BAD" NEIGHBOR
+    Neighboring Hotspot           | dist km | heading | bad RSSI (%)  | bad SNR (%)   |
+    ------------------------------+---------+---------+---------------+---------------|
+    tricky-carbon-pheasant        |   4.5   |  10  N  |  0/ 10 (  0%) |  5/ 10 ( 50%) |
+    polite-silver-bat             |   0.6   |  30 NE  |  0/ 24 (  0%) |  1/ 24 (  4%) |
+    faint-pearl-mantis            |   0.5   |  60 NE  |  0/ 25 (  0%) |  1/ 25 (  4%) |
+    big-gunmetal-blackbird        |   4.5   |  10  N  |  0/  6 (  0%) |  5/  6 ( 83%) |
+    shallow-parchment-copperhead  |   4.5   |  10  N  |  0/ 12 (  0%) |  5/ 12 ( 42%) |
+    skinny-fiery-wallaby          |   3.4   | 350  N  |  0/ 11 (  0%) |  2/ 11 ( 18%) |
+    swift-purple-griffin          |   2.1   | 270  W  |  0/  6 (  0%) |  5/  6 ( 83%) |
+    clean-leather-stallion        |   1.3   | 310 NW  |  0/ 20 (  0%) |  3/ 20 ( 15%) |
+    innocent-maroon-swift         |   1.0   | 315 NW  |  0/ 11 (  0%) |  1/ 11 (  9%) |
+    uneven-banana-peacock         |   4.5   |  10  N  |  0/  3 (  0%) |  2/  3 ( 67%) |
+    large-mercurial-coyote        |   2.6   | 285  W  |  0/  7 (  0%) |  1/  7 ( 14%) |
+
+ This table gives distance between hotspots, heading (degrees and compass) from reference hotspot and the breakdown of RSSI or SNR failures.
+
+
 ### poc_reliability
-Second is a report `poc_reliability` which analyzes the reliability of the specified hotsot receiving PoCs from its neighbors as well as transmitting to neighbors.
+Second is a report `poc_reliability` which analyzes the reliability of the specified hotpsot receiving PoCs from its neighbors as well as transmitting to neighbors.
 With the current PoC reward system transmitting to the following hotspot is less important since transmissions are rewarded as long as any hotspot receives the transmission (witness or next hop).
-If your hotspot cannot reliably receive challenges from its neighbors it is losing out on rewards.
+If your hotspot cannot reliably receive challenges from its neighbors it is losing out on rewards.  
+
+**Note**: This does not take into account receives that fail PoCv10 thresholds.  "recv" means the signal was received, not that the receipt contains valid rssi/snr.
 
 To run the `poc_reliability` report run:
 
-    python3 analyze_hotspot.py -x poc_reliability -a {hotspot address}
+    python3 analyze_hotspot.py -x poc_reliability -address {hotspot address}
     
     
 An example output table is:
 
     analyzing 500 challenges from block 516256-498146 over 11 days, 11 hrs
     PoC hops from: name-name-name
-    to receiving hotspot           | dist km | heading | pass/ttl | pass % |
+    to receiving hotspot           | dist km | heading | recv/ttl | recv % |
     ------------------------------------------------------------------------
     tangy-aegean-grasshopper       |    0.4  |   15  N |  26/ 29  |    90% |
     warm-hotpink-monkey            |    0.5  |  340  N |   5/  6  |    83% |
@@ -81,7 +108,7 @@ An example output table is:
     
     
     PoC hops to: name-name-name
-    from transmitting hotspot      | dist km | heading | pass/ttl | pass % |
+    from transmitting hotspot      | dist km | heading | recv/ttl | recv % |
     ------------------------------------------------------------------------
     warm-hotpink-monkey            |    0.5  |  340  N |  16/ 26  |    62% |
     trendy-fleece-cobra            |    2.5  |  100  E |   2/  6  |    33% |
@@ -90,6 +117,6 @@ An example output table is:
     other (10)                     |  2.2-33 |   N/A   |   1/ 10  |    10% | 
     
  
-These tables count the total number of times the hotspot failed to receive an RF transmission (meaning previous hop passed) as well as the number of times the target hotspot passed and the following hop failed to receive.
+These tables count the total number of times the hotspot failed to receive an RF transmission (meaning previous hop passed) as well as the number of times the target hotspot transmitted and the following hop failed to receive.
 For hotspots with lots of interactions, any neighbor with a total of 1 interaction is pooled together into an "other" category.
 The columns in this table give the distance between the two hotspots in km, the heading from the reference hotspot to neighbor in degrees and compass heading as well as the total interaction counts and pass percentage.
--- analyze_hotspot.py
+++ analyze_hotspot.py
@@ -8,6 +8,12 @@ import argparse
 import utils
 from classes.Hotspots import Hotspots
 
+def __heading2str__(heading):
+    headingstr = ['N', 'NE', 'E', 'SE', 'S', 'SW', 'W', 'NW']
+    heading = 5 * round(heading / 5, 0)
+    idx = int(round(heading / 45)) % 8
+
+    return f"{heading:3.0f} {headingstr[idx]:>2}"
 
 
 def pocv10_violations(hotspot, chals):
@@ -39,11 +45,15 @@ def pocv10_violations(hotspot, chals):
                     rssi_lim = utils.max_rssi(dist)
                     snr_rssi_lim = utils.snr_min_rssi(w['snr'])
                     transmits_w['total'] += 1
+                    if w['gateway'] not in bad_neighbors:
+                        bad_neighbors[w['gateway']] = dict(rssi=0, snr=0, ttl=0)
+                    bad_neighbors[w['gateway']]['ttl'] += 1
                     if w['signal'] > rssi_lim:
                         transmits_w['bad_rssi'] += 1
+                        bad_neighbors[w['gateway']]['rssi'] += 1
                     if w['signal'] < snr_rssi_lim:
                         transmits_w['bad_snr'] += 1
-
+                        bad_neighbors[w['gateway']]['snr'] += 1
                 if p['receipt'] and transmitter:
                     dist = utils.haversine_km(
                         hlat, hlng,
@@ -52,15 +62,14 @@ def pocv10_violations(hotspot, chals):
                     rssi_lim = utils.max_rssi(dist)
                     snr_rssi_lim = utils.snr_min_rssi(p['receipt']['snr'])
                     poc_rcv['total'] += 1
+                    if transmitter not in bad_neighbors:
+                        bad_neighbors[transmitter] = dict(rssi=0, snr=0, ttl=0)
+                    bad_neighbors[transmitter]['ttl'] += 1
                     if p['receipt']['signal'] > rssi_lim:
                         poc_rcv['bad_rssi'] += 1
-                        if transmitter not in bad_neighbors:
-                            bad_neighbors[transmitter] = dict(rssi=0, snr=0)
                         bad_neighbors[transmitter]['rssi'] += 1
                     if p['receipt']['signal'] < snr_rssi_lim:
                         poc_rcv['bad_snr'] += 1
-                        if transmitter not in bad_neighbors:
-                            bad_neighbors[transmitter] = dict(rssi=0, snr=0)
                         bad_neighbors[transmitter]['snr'] += 1
                         print(f"failed snr with lim {snr_rssi_lim}")
                         print(p['receipt'])
@@ -77,26 +86,44 @@ def pocv10_violations(hotspot, chals):
                     rssi_lim = utils.max_rssi(dist)
                     snr_rssi_lim = utils.snr_min_rssi(w['snr'])
                     receives_w['total'] += 1
+                    if p['challengee'] not in bad_neighbors:
+                        bad_neighbors[p['challengee']] = dict(rssi=0, snr=0, ttl=0)
+                    bad_neighbors[p['challengee']]['ttl'] += 1
                     if w['signal'] > rssi_lim:
                         receives_w['bad_rssi'] += 1
-                        if p['challengee'] not in bad_neighbors:
-                            bad_neighbors[p['challengee']] = dict(rssi=0, snr=0)
                         bad_neighbors[p['challengee']]['rssi'] += 1
                     if w['signal'] < snr_rssi_lim:
                         receives_w['bad_snr'] += 1
-                        if p['challengee'] not in bad_neighbors:
-                            bad_neighbors[p['challengee']] = dict(rssi=0, snr=0)
                         bad_neighbors[p['challengee']]['snr'] += 1
             transmitter = p['challengee']
     print(f"analyzed {len(chals)} challenges from {chals[0]['height']}-{chals[-1]['height']}")
     print(f"PoC v10 failures for {hotspot['name']}")
+
+    print(F"SUMMARY")
     print(f"Category                   | Total | bad RSSI (%) | bad SNR (%) |")
     print(f"-----------------------------------------------------------------")
     print(f"Witnesses to hotspot >300m | {transmits_w['total']:5d} | {transmits_w['bad_rssi']:4d} ({transmits_w['bad_rssi']*100/max(1, transmits_w['total']):3.0f}%)  | {transmits_w['bad_snr']:4d} ({transmits_w['bad_snr']*100/max(1, transmits_w['total']):3.0f}%) |")
     print(f"Hotspot witnessing  >300m  | {receives_w['total']:5d} | {receives_w['bad_rssi']:4d} ({receives_w['bad_rssi']*100/max(1, receives_w['total']):3.0f}%)  | {receives_w['bad_snr']:4d} ({receives_w['bad_snr']*100/max(1, receives_w['total']):3.0f}%) |")
     print(f"Hotspot PoC receipts       | {poc_rcv['total']:5d} | {poc_rcv['bad_rssi']:4d} ({poc_rcv['bad_rssi']*100/max(1, poc_rcv['total']):3.0f}%)  | {poc_rcv['bad_snr']:4d} ({poc_rcv['bad_snr']*100/max(1, poc_rcv['total']):3.0f}%) |")
 
+    print()
+    print()
+    print(f'BY "BAD" NEIGHBOR')
+    print(f"Neighboring Hotspot           | dist km | heading | bad RSSI (%)  | bad SNR (%)   |")
+    print(f"------------------------------+---------+---------+---------------+---------------|")
+    hlat, hlng = hotspot['lat'], hotspot['lng']
+    for n in bad_neighbors:
+        if bad_neighbors[n]['rssi'] or bad_neighbors[n]['snr']:
+            bad_h = H.get_hotspot_by_addr(n)
+            dist_km, heading = utils.haversine_km(
+                hlat,
+                hlng,
+                bad_h['lat'],
+                bad_h['lng'],
+                return_heading=True
+            )
 
+            print(f"{H.get_hotspot_by_addr(n)['name']:29} | {dist_km:5.1f}   | {__heading2str__(heading):7} | {bad_neighbors[n]['rssi']:2d}/{bad_neighbors[n]['ttl']:3d} ({bad_neighbors[n]['rssi']*100/bad_neighbors[n]['ttl']:3.0f}%) | {bad_neighbors[n]['snr']:2d}/{bad_neighbors[n]['ttl']:3d} ({bad_neighbors[n]['snr']*100/bad_neighbors[n]['ttl']:3.0f}%) |")
 
 def poc_reliability(hotspot, challenges):
     """
@@ -123,20 +150,20 @@ def poc_reliability(hotspot, challenges):
         for p in chal['path'][:-1][::-1]:
             if pnext_pass or p['witnesses'] or p['receipt']:
                 if pnext['challengee'] == haddr:
-                    if p['challengee'] not in results_tx:
-                        results_tx[p['challengee']] = [0, 0]
-                    results_tx[p['challengee']][0 if pnext_pass else 1] += 1
+                    if p['challengee'] not in results_rx:
+                        results_rx[p['challengee']] = [0, 0]
+                    results_rx[p['challengee']][0 if pnext_pass else 1] += 1
                 if p['challengee'] == haddr:
-                    if pnext['challengee'] not in results_rx:
-                        results_rx[pnext['challengee']] = [0, 0]
-                    results_rx[pnext['challengee']][0 if pnext_pass else 1] += 1
+                    if pnext['challengee'] not in results_tx:
+                        results_tx[pnext['challengee']] = [0, 0]
+                    results_tx[pnext['challengee']][0 if pnext_pass else 1] += 1
                 pnext_pass = True
             pnext = p
 
     hlat = hotspot['lat']
     hlon = hotspot['lng']
 
-    def summary_table(results, is_tx=False):
+    def summary_table(results, hotspot_transmitting=False):
 
         other_pass = 0
         other_ttl = 0
@@ -144,12 +171,12 @@ def poc_reliability(hotspot, challenges):
         dist_min = 9999
         dist_max = 0
 
-        if is_tx:
+        if hotspot_transmitting:
             print(f"PoC hops from: {hotspot['name']}")
-            print(f"{'to receiving hotspot':30} | {'dist km'} | {'heading'} | pass/ttl | pass % |")
+            print(f"{'to receiving hotspot':30} | {'dist km'} | {'heading'} | recv/ttl | recv % |")
         else:
             print(f"PoC hops to: {hotspot['name']}")
-            print(f"{'from transmitting hotspot':30} | {'dist km'} | {'heading'} | pass/ttl | pass % |")
+            print(f"{'from transmitting hotspot':30} | {'dist km'} | {'heading'} | recv/ttl | recv % |")
         print("-" * 72)
 
         for h in results.keys():
@@ -165,7 +192,7 @@ def poc_reliability(hotspot, challenges):
             idx = int(round(heading / 45)) % 8
             headingstr = ['N', 'NE', 'E', 'SE', 'S', 'SW', 'W', 'NW']
             if ttl == 1:
-                other_ttl += 1
+                other_ttl += ttl
                 other_pass += results[h][0]
                 other_cnt += 1
                 dist_min = min(dist_min, dist)
@@ -177,10 +204,10 @@ def poc_reliability(hotspot, challenges):
         if other_ttl:
             print(f"other ({other_cnt:2}){' ' * 20} | {dist_min:4.1f}-{dist_max:2.0f} |   N/A   | {other_pass:3d}/{other_ttl:3d}  | {other_pass / other_ttl * 100:5.0f}% | ")
 
-    summary_table(results_tx, is_tx=True)
+    summary_table(results_tx, hotspot_transmitting=True)
     print()
     print()
-    summary_table(results_rx, is_tx=False)
+    summary_table(results_rx, hotspot_transmitting=False)
 
 def main():
     parser = argparse.ArgumentParser("analyze hotspots", add_help=True)
